c4241da996ecfc36380f3b156c3be281ff2de2f9
NTorres67 <ntorres67@sbcglobal.net>
2018-02-06 23:08:56 -0500

    Added to Program Class

--- FortuneTellerMethod/FortuneTelling.cs
+++ FortuneTellerMethod/FortuneTelling.cs
@@ -14,9 +14,9 @@ namespace FortuneTellerMethod
         private string vac;
         private string col;
         private string pay;
-       
-        
-        
+
+
+
 
         // Properties
         public string NameFirst
@@ -57,7 +57,13 @@ namespace FortuneTellerMethod
 
 
 
-        // Constructors
+        //Constructors
+        public FortuneTelling()
+        {
+            //default constructor
+        }
+
+
         public FortuneTelling(string nameFirst, string nameLast, int retire, string vac, string col, string pay)
         {
             this.nameFirst = nameFirst;
@@ -66,7 +72,7 @@ namespace FortuneTellerMethod
             this.vac = vac;
             this.col = col;
             this.pay = pay;
-            
+
         }
 
 
@@ -74,46 +80,243 @@ namespace FortuneTellerMethod
         // Methods
 
         // Greet user
-        public string Greeting()
+        public void Greeting()
         {
-            return ("Greetings " + nameFirst + " " + nameLast + ". " + "I will tell you your fortune!");
+            // User's First Name
+            string nameFirst = "";
+            Console.WriteLine("Please enter your first name.");
+            nameFirst = Console.ReadLine();
+            Console.WriteLine(" ");
+
+            // User's Last Name
+            string nameLast = "";
+            Console.WriteLine("Please enter your last name.");
+            nameLast = Console.ReadLine();
+            Console.WriteLine(" ");
+
+            // User's Full Name
+            string nameFull = nameFirst + nameLast;
+
+            // Greet User
+            Console.WriteLine("Greetings, " + nameFirst + " " + nameLast + ". I will tell you your fortune!");
+            //Console.WriteLine("Greetings, " + nameFirst + " " + nameLast + ". I will tell you your fortune!");
+            Console.WriteLine("");
+        }
+
+        public void FirstName()
+        {
+            Console.WriteLine(nameFirst);
+        }
+
+        public void LastName()
+        {
+            Console.WriteLine(nameLast);
         }
 
 
         // Calculate years to retirement
         public int ToRetirement()
         {
+            //User's Age
+            Console.WriteLine("Please enter your age.");
+            int ageCurrent = int.Parse(Console.ReadLine());
+            Console.WriteLine(" ");
+            //Console.WriteLine(ageCurrent);
+
+            //Years to Retirement
+            int ageOdd = 62;
+            int ageEven = 65;
+            //int retire = (ageEven - ageCurrent);
+
+            int retire;
+            if ((ageCurrent % 2) == 0)
+            {
+                retire = (ageEven - ageCurrent);
+                //Console.WriteLine(retire);
+            }
+            else
+            {
+                retire = (ageOdd - ageCurrent);
+            }
+
+            Console.WriteLine("");
+
+            //Console.WriteLine(retire);
             return (retire);
         }
 
-        
+
+
         // Determine vacation home utilizing number of siblings
         public string VacationHome()
         {
+            //Number Siblings
+            Console.WriteLine("How many sibilings do you have?");
+            int numberSiblings = int.Parse(Console.ReadLine());
+            //Console.Write(numberSiblings);
+            Console.WriteLine("");
+
+            //Vacation Home according to number of siblings
+            string vac = "";
+
+            if (numberSiblings <= 0)
+            {
+                vac = "shack int the garbage dump";
+                //Console.WriteLine(vac);
+            }
+            if (numberSiblings == 0)
+            {
+                vac = "cabin in the woods";
+            }
+            if (numberSiblings == 1)
+            {
+                vac = "dome in the Pacific Ocean";
+            }
+            if (numberSiblings == 2)
+            {
+                vac = "houseboat in the Mediterranean";
+            }
+            if (numberSiblings == 3)
+            {
+                vac = "yurt in the Rockies";
+            }
+            if (numberSiblings >= 3)
+            {
+                vac = "yurt in the Rockies";
+            }
+            else
+            { }
+
+            Console.WriteLine("");
+
+            //Console.WriteLine(vac);
             return (vac);
         }
-        
+
 
 
         // Mode of transportation utilizing ROYGBIV
         public string FavoriteColor()
         {
+            //Favorite Color
+            Console.WriteLine("Please state your favorite using the ROYGBIV color acronym.");
+            Console.WriteLine("If help is required, please type the word \"Help.\"");
+            string favoriteColor = Console.ReadLine();
+            //Console.WriteLine(favoriteColor);
+            string color = favoriteColor.ToLower();
+            Console.WriteLine(" ");
+
+            if (favoriteColor == "help")
+            {
+                Console.WriteLine("R = red");
+                Console.WriteLine("O = orange");
+                Console.WriteLine("Y = yellow");
+                Console.WriteLine("G = green");
+                Console.WriteLine("B = blue");
+                Console.WriteLine("I = indigo");
+                Console.WriteLine("V = violet");
+
+                Console.WriteLine("");
+
+                Console.WriteLine("Please select your favorit color using the ROYGIBIV color acronym.");
+                favoriteColor = Console.ReadLine();
+                color = favoriteColor.ToLower();
+            }
+
+            Console.WriteLine("");
+
+            //Transportation according to favorite color
+            string col = "";
+
+            switch (color)
+            {
+                case "red":
+                    col = "your feet";
+                    //Console.WriteLine(red);
+                    break;
+
+                case "orange":
+                    col = "horseless wagon";
+                    break;
+
+                case "yellow":
+                    col = " truck";
+                    break;
+
+                case "green":
+                    col = "scooter";
+                    break;
+
+                case "blue":
+                    col = "donkey";
+                    break;
+
+                case "indigo":
+                    col = "seahorse";
+                    break;
+
+                case "violet":
+                    col = "sled pulled by rabbits";
+                    break;
+
+                default:
+                    break;
+            }
+            Console.WriteLine("");
+
+            //Console.WriteLine(col);
             return (col);
         }
-
-
 
         // Determine how much money will retire with as determined by month of birth
         public string RetirementPay()
         {
+            // Birth Month
+            Console.WriteLine("Please enter your month of birth by its numerical value.");
+            int birthMonth = int.Parse(Console.ReadLine());
+            //Console.WriteLine(birthMonth);
+            Console.WriteLine(" ");
+
+            // Money in bank based on month of birth
+            string pay = "";
+            if ((birthMonth == 1) || (birthMonth == 2) || (birthMonth == 3) || (birthMonth == 4))
+            {
+                pay = "$10,000";
+                //Console.WriteLine(payOne);
+            }
+            if ((birthMonth == 5) || (birthMonth == 6) || (birthMonth == 7) || (birthMonth == 8))
+            {
+                pay = "$15,000";
+                //Console.WriteLine(payTwo);
+            }
+            if ((birthMonth == 9) || (birthMonth == 10) || (birthMonth == 11) || (birthMonth == 12))
+            {
+                pay = "$20,000";
+                //Console.WriteLine(payThree);
+            }
+            if ((birthMonth <= 0) || (birthMonth >= 12))
+            {
+                pay = "$0.00";
+                //Console.WriteLine(payOther);
+            }
+            else
+            { }
+            Console.WriteLine("");
+
+            //Console.WriteLine(pay);
             return (pay);
         }
 
 
         // Judge how good the fortune is
-        public void  Judgement()
+        public void Judgement()
         {
             Console.WriteLine("Better luck in your next life! ( *_* )");
+        }
+
+        public void QuitProgram()
+        {
+
         }
 
 
--- FortuneTellerMethod/Program.cs
+++ FortuneTellerMethod/Program.cs
@@ -10,199 +10,44 @@ namespace FortuneTellerMethod
     {
         static void Main(string[] args)
         {
-            string nameFirst = "";
-            string nameLast = "";
+            
 
-            //User's First Name
-            Console.WriteLine("Please enter your first name.");
-            nameFirst = Console.ReadLine();
-            Console.WriteLine(" ");
-            //Console.WriteLine(nameFirst);
+            FortuneTelling userFortuneTelling = new FortuneTelling();
 
-            //User's Last Name
-            Console.WriteLine("Please enter your last name.");
-            nameLast = Console.ReadLine();
-            Console.WriteLine(" ");
-            //Console.WriteLine(nameLast);
 
-            //User's Age
-            Console.WriteLine("Please enter your age.");
-            int ageCurrent = int.Parse(Console.ReadLine());
-            Console.WriteLine(" ");
-            //Console.WriteLine(ageCurrent);
 
-            //Years to Retirement
-            int ageOdd = 62;
-            int ageEven = 65;
-            //int retire = (ageEven - ageCurrent);
+            //// Greet User
 
-            int retire;
-            if ((ageCurrent % 2) == 0)
-            {
-                retire = (ageEven - ageCurrent);
-                //Console.WriteLine(retire);
-            }
-            else
-            {
-                retire = (ageOdd - ageCurrent);
-            }
+            userFortuneTelling.Greeting();
 
-            //Console.WriteLine(retire);
-               Console.WriteLine("");
+            userFortuneTelling.ToRetirement();
 
-            //Birth Month
-            Console.WriteLine("Please enter your month of birth by its numerical value.");
-            int birthMonth = int.Parse(Console.ReadLine());
-            //Console.WriteLine(birthMonth);
-            Console.WriteLine(" ");
+            userFortuneTelling.VacationHome();
 
-            //Money in bank based on month of birth
-            string pay = "";
-            if ((birthMonth == 1) || (birthMonth == 2) || (birthMonth == 3) || (birthMonth == 4))
-            {
-                pay = "$10,000";
-                //Console.WriteLine(payOne);
-            }
-            if ((birthMonth == 5) || (birthMonth == 6) || (birthMonth == 7) || (birthMonth == 8))
-            {
-                pay = "$15,000";
-                //Console.WriteLine(payTwo);
-            }
-            if ((birthMonth == 9) || (birthMonth == 10) || (birthMonth == 11) || (birthMonth == 12))
-            {
-                pay = "$20,000";
-                //Console.WriteLine(payThree);
-            }
-            if ((birthMonth <= 0) || (birthMonth >= 12))
-            {
-                pay = "$0.00";
-                //Console.WriteLine(payOther);
-            }
-            else
-            { }
-            Console.WriteLine("");
+            userFortuneTelling.FavoriteColor();
 
-            //Favorite Color
-            Console.WriteLine("Please state your favorite using the ROYGBIV color acronym.");
-            Console.WriteLine("If help is required, please type the word \"Help.\"");
-            string favoriteColor = Console.ReadLine();
-            //Console.WriteLine(favoriteColor);
-            string color = favoriteColor.ToLower();
-            Console.WriteLine(" ");
+            userFortuneTelling.RetirementPay();
 
-            if (favoriteColor == "help")
-            {
-                Console.WriteLine("R = red");
-                Console.WriteLine("O = orange");
-                Console.WriteLine("Y = yellow");
-                Console.WriteLine("G = green");
-                Console.WriteLine("B = blue");
-                Console.WriteLine("I = indigo");
-                Console.WriteLine("V = violet");
+            userFortuneTelling.FirstName();
 
-                Console.WriteLine("");
 
-                Console.WriteLine("Please select your favorit color using the ROYGIBIV color acronym.");
-                favoriteColor = Console.ReadLine();
-                color = favoriteColor.ToLower();
-            }
+            //// Give Fortune
 
-            Console.WriteLine("");
+            Console.WriteLine();
+            
 
-            //Transportation according to favorite color
-            string col = "";
-            //string orange = "";
-            //string yellow = "";
-            //string green = "";
-            //string blue = "";
-            //string indigo = "";
-            //string violet = "";
+           
 
-            switch (color)
-            {
-                case "red":
-                    col = "your feet";
-                    //Console.WriteLine(red);
-                    break;
+            //// Judge Fortune
+            userFortuneTelling.Judgement();
+            
 
-                case "orange":
-                    col = "horseless wagon";
-                    break;
+          
 
-                case "yellow":
-                    col = " truck";
-                    break;
-
-                case "green":
-                    col = "scooter";
-                    break;
-
-                case "blue":
-                    col = "donkey";
-                    break;
-
-                case "indigo":
-                    col = "seahorse";
-                    break;
-
-                case "violet":
-                    col = "sled pulled by rabbits";
-                    break;
-
-                default:
-                    break;
-            }
-
-            Console.WriteLine("");
-
-            //Number Siblings
-            Console.WriteLine("How many sibilings do you have?");
-            int numberSiblings = int.Parse(Console.ReadLine());
-            //Console.Write(numberSiblings);
-            Console.WriteLine("");
-
-            //Vacation Home according to number of siblings
-            string vac = "";
-            //string zero = "";
-            //string one = "";
-            //string two = "";
-            //string three = "";
-
-            if (numberSiblings <= 0)
-            {
-                vac = "shack int the garbage dump";
-                //Console.WriteLine(vac);
-            }
-            if (numberSiblings == 0)
-            {
-                vac = "cabin in the woods";
-            }
-            if (numberSiblings == 1)
-            {
-                vac = "dome in the Pacific Ocean";
-            }
-            if (numberSiblings == 2)
-            {
-                vac = "houseboat in the Mediterranean";
-            }
-            if (numberSiblings == 3)
-            {
-                vac = "yurt in the Rockies";
-            }
-            if (numberSiblings >= 3)
-            {
-                vac = "yurt in the Rockies";
-            }
-            else
-            { }
-
-            Console.WriteLine("");
 
         }
 
-
         
-
     }
 }
 
